5e21210e310f61dd2166939da12ea1af37888410
robin <robin.guignardperret@gmail.com>
2016-11-22 15:08:17 +0100

    game simulator

--- second_round/ex00_conway/conway.cpp
+++ second_round/ex00_conway/conway.cpp
@@ -3,6 +3,8 @@
 #include <map>
 using namespace std;
 
+#define FIGX 15
+#define FIGY 15
 
 class coord
 {
@@ -71,6 +73,7 @@ std::vector< coord > neighbour_available(std::vector< std::string > board, coord
     return (is_neighbour(board, pos, '-'));
 }
 
+
 bool    is_corner(std::vector< std::string > board, coord pos)
 {
     if( (pos.x == 0 && pos.y == 0)
@@ -101,6 +104,19 @@ std::vector< coord > neighbour_unavailable(std::vector< std::string > board, coo
     
 }
 
+std::vector< coord >    neighbour_cells(std::vector< std::string > board)
+{
+    std::vector<coord>  res;
+    for (int x = 0; x < board.size(); x++)
+    {
+        for (int y = 0; y < board[0].size(); y++)
+        {
+            if(neighbour_unavailable(board, coord(x, y), 'r').size() != 0)
+                res.push_back(coord(x, y));
+        }
+    }
+    return(res);
+}
 std::vector< coord >    check_exploder(std::vector<std::string> board, char player)
 {
     std::vector< coord >    res;
@@ -117,12 +133,12 @@ std::vector< coord >    check_exploder(std::vector<std::string> board, char play
     res.push_back(coord(15, 21));
 */    /**/
     /* heart patern */
-    res.push_back(coord(15, 15));
-    res.push_back(coord(15, 17));
-    res.push_back(coord(16, 15));
-    res.push_back(coord(16, 16));
-    res.push_back(coord(16, 17));
-    res.push_back(coord(17, 16));
+    res.push_back(coord(FIGX, FIGY));
+    res.push_back(coord(FIGX, FIGY + 2));
+    res.push_back(coord(FIGX + 1, FIGY));
+    res.push_back(coord(FIGX + 1, FIGY + 1));
+    res.push_back(coord(FIGX + 1, FIGY + 2));
+    res.push_back(coord(FIGX + 2, FIGY + 1));
     
     for (int i = 0; i < res.size(); i++)
     {
@@ -173,12 +189,23 @@ void    suicide_penalty(std::vector<std::string> board, std::vector< coord > fre
 
 void    birthplace_bonus(std::vector<std::string> board, std::vector< coord > free_nb, std::map< std::string, int > &result, int inc, char player)
 {
+    std::vector <coord> nb;
     for(int i = 0; i < free_nb.size(); i++)
     {
-        if(is_neighbour(board, free_nb[i], (player == 'w') ? 'b' : 'w').size() != 1 && neighbour_unavailable(board, free_nb[i], player).size() == 3)
+        if(is_neighbour(board, free_nb[i], player).size() != 1 && neighbour_unavailable(board, free_nb[i], player).size() == 3)
         {
             std::cerr << "birth place at:" << free_nb[i].serialize() << std::endl;
             result[free_nb[i].serialize()] = result[free_nb[i].serialize()] + inc;
+        }
+        else if(is_neighbour(board, free_nb[i], player).size() == 2 && neighbour_unavailable(board, free_nb[i], player).size() == 2)
+        {
+            nb = neighbour_available(board, free_nb[i]);
+            incr_score_neighbour(nb, result, 1); // ?
+        }
+        else if(is_neighbour(board, free_nb[i], player).size() == 3 && neighbour_unavailable(board, free_nb[i], player).size() == 3)
+        {
+            nb = neighbour_available(board, free_nb[i]);
+            incr_score_neighbour(nb, result, -inc); // ?
         }
     }
 }
@@ -205,12 +232,12 @@ bool    print_deadly(std::vector< std::string > board, std::vector< coord > them
         free_nb = neighbour_available(board, us[i]);
         not_free_nb = neighbour_unavailable(board, us[i], player);
         if (not_free_nb.size() == 1)
-            incr_score_neighbour(free_nb, result, 10);
+            incr_score_neighbour(free_nb, result, 99);
         else if (not_free_nb.size() < 4)
             incr_score_neighbour(free_nb, result, -1);
     }
     suicide_penalty(board, available, result, -100, player);
-    birthplace_bonus(board, available, result, 100, player);
+    birthplace_bonus(board, available, result, 99, player);
     
     typedef std::map< std::string, int >::iterator it_type;
     for(it_type it = result.begin(); it != result.end(); it++)
@@ -234,7 +261,86 @@ bool    print_deadly(std::vector< std::string > board, std::vector< coord > them
         return(0);
 }
 
-//int simulate_game(player, board)
+void    sig_empty(const std::vector<std::string> board, std::vector<std::string> &new_board, coord pos, char player)
+{
+    std::vector<coord>  us;
+    std::vector<coord>  them;
+    char                them_c;
+    
+    them_c = (player == 'w') ? 'b' : 'w';
+    us = is_neighbour(board, pos, player);
+    them = is_neighbour(board, pos, them_c);
+    if (us.size() + them.size() == 3)
+        new_board[pos.x][pos.y] = (us.size() < them.size()) ? them_c : player;
+}
+
+void    sig_filled(const std::vector<std::string> board, std::vector<std::string> &new_board, coord pos)
+{
+    unsigned int    neighbours;
+    
+    neighbours = is_neighbour(board, pos, 'w').size() + is_neighbour(board, pos, 'b').size();
+    if (neighbours != 3 && neighbours != 2)
+        new_board[pos.x][pos.y] = '-';
+}
+
+std::vector<std::string>    simulate_step(std::vector<std::string> board, char player)
+{
+    coord   pos;
+    std::vector<std::string>     new_board;
+    
+    new_board = board;
+    for (pos.x = 0; pos.x < board.size(); pos.x++)
+    {
+        for (pos.y = 0; pos.y < board[0].size(); pos.y++)
+        {
+            if(board[pos.x][pos.y] == '-')
+                sig_empty(board, new_board, pos, player);
+            else if(board[pos.x][pos.y] == 'w' || board[pos.x][pos.y] == 'b')
+                sig_filled(board, new_board, pos);
+        }
+    }
+    return (new_board);
+}
+
+int     simulate_game(std::vector<std::string> board, char player, int depth)
+{
+    int     score;
+    
+    score = 0;
+    
+    for(int i = 0; i < depth; i++)
+        board = simulate_step(board, player);
+    
+    score = find(board, player).size() - find(board, (player == 'w') ? 'b' : 'w').size();
+    return (score);
+}
+
+bool    best_move(std::vector<std::string> board, std::vector<coord> moves, char player)
+{
+    std::vector<string> board_test;
+    int     max;
+    int     temp;
+    coord   max_coord;
+    
+    max = -10;
+    max_coord.x = moves[0].x;
+    max_coord.y = moves[0].y;
+    
+    for (int i = 0; i < moves.size(); i++)
+    {
+        board_test = board;
+        board_test[moves[i].x][moves[i].y] = player;
+        temp = simulate_game(board, player, 1);
+        std::cerr << "score: " << temp << std::endl;
+        if(temp > max)
+        {
+            max = temp;
+            max_coord = moves[i];
+        }
+    }
+    std::cout << max_coord.x << " " << max_coord.y << std::endl;
+    return (true); 
+}
 
 void nextMove(char player, vector <string> board)
 {
@@ -243,19 +349,20 @@ void nextMove(char player, vector <string> board)
     std::vector< coord >    available;
     std::vector< coord >    exploder;
 
-    us = find(board, player);
+//    us = find(board, player);
     them = find(board, (player == 'w') ? 'b' : 'w');
     available = find(board, '-');
-    exploder = check_exploder(board, player);
+//    exploder = check_exploder(board, player);
 
-    std::cerr << "nb_them: " << them.size() << std::endl;
     if (them.size() == 0)
     {
-        std::cout << 15 << " " <<  15 << std::endl;
+        std::cout << FIGX << " " <<  FIGY << std::endl;
         return ;
     }
-    if (print_first_av(board, exploder)) return ;
+    std::cerr << neighbour_cells(board).size() << std::endl;
+    if (best_move(board, neighbour_cells(board), player)) return ;
     if (print_deadly(board, them, us, player, available)) return ;
+    if (print_first_av(board, exploder)) return ;
     std::cout << available[available.size()/5].x << " " <<  available[available.size()/5].y << std::endl;
     
     return ;
